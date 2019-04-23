6e0b567d70697fcde87364493cd6b9f7d0b98d4d
StratosBlur <wave_onlyone@gmail.com>
2019-04-24 03:39:15 +0700

    update config productuin

--- src/config.js
+++ src/config.js
@@ -1,5 +1,10 @@
 const config = {
-	api_uri: 'http://localhost:2019',
+	dev: { api_uri: 'http://localhost:2019' },
+	product: { api_uri: 'http://172.96.190.138:2019' },
 };
+let env = 'dev';
+if (window.location.href !== 'http://localhost:3000/') {
+	env = 'product';
+}
 
-export default config;
+export default config[env];
