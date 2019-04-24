80116e1c6482a111082e7c867da8e6b024e4da2d
StratosBlur <wave_onlyone@gmail.com>
2019-04-25 01:11:44 +0700

    fix port on production

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 const config = {
 	dev: { api_uri: 'http://localhost:2019' },
-	product: { api_uri: 'http://172.96.190.138:2019' },
+	product: { api_uri: 'https://pp.10000lan.com/backend' },
 };
 let env = 'dev';
 if (window.location.href !== 'http://localhost:3000/') {
